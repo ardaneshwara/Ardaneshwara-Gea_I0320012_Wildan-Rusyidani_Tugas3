54392eb3d8e333e9dc1c53b02d51c92cd49ede3e
ardaneshwara <ardaneshwaragea2@gmail.com>
2021-03-17 19:59:12 +0700

    Pengerjaan Exercise week 3

---
+++ I0320012_soal2_tugas3.py
@@ -0,0 +1,13 @@
+# Membuat dictionary pada python
+dict = {'Nama':'Gea','Hobi1':'Fotografi','Hobi2':'Mendengarkan musik','Hobi3':'Dance','Sosmed1':'ig:ardaneshwaragea','Sosmed2':'line:geaines','Sosmed3':'twitter:rtnsbv','Makanan1':'ayam cabai garam','Makanan2':'steak','Makanan3':'pizza'}
+
+# Mengubah salah satu hobi dan sosmed, menghapus 2 makanan, dan menambah 1 hobi
+dict['Hobi3'] = 'Dance'
+dict['Sosmed2'] = 'line:geaines'
+del dict['Makanan1']
+del dict['Makanan2']
+dict['Hobi4'] = 'Melihat drama korea'
+
+print(dict)
+
+
