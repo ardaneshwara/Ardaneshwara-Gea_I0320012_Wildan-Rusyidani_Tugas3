c74d9a2cb12cc399de708094f9982ff1f9f44f9f
ardaneshwara <ardaneshwaragea2@gmail.com>
2021-03-19 06:26:19 +0700

    Pengerjaan Exercise week 3

--- I0320012_soal1_tugas3.py
+++ I0320012_soal1_tugas3.py
@@ -1,20 +1,24 @@
 # Menampilkan list 10 teman
-list = ['Deadila','Bagus','Aratia','Divana','Alifiana','Funny','Ayu','Hana','Candrika','Angela']
+list = ['Deadila','Bagus','Sekar','Divana','Alifiana','Funny','Ayu','Hana','Candrika','Angela']
 
 # Menampilkan isi list indeks nomor 4, 6, dan 7
 print("List indeks nomor 4,6, dan 7 yaitu", list[4],",", list[6], ",","dan", list[7])
 
 # Mengganti nama teman di list 3, 5, 9
-list[3] = 'Sekar'
+list[3] = 'Aratia'
 list[5] = 'Cita'
-list[6] = 'Attar'
+list[6] = 'Tsania'
 
 # Menambahkan 2 nama teman
 list.append('Maurich')
 list.append('Zafira')
 
 # Menampilkan semua teman dengan pengulangan
-print(list*2)
+thaitea = 0
+for matcha in range (0,12) :
+    print (list[thaitea])
+    thaitea+=1
 
 # Menampilkan panjang list
-print(len(list))
+print("Panjang indeks list adalah", len(list))
+
